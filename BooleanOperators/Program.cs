b880a5216a277681fd090bbc4346d3a3d10418ab
Cnix24 <calvinattl24@gmail.com>
2019-07-08 16:03:01 -0600

    Udpate
    Update

---
+++ BooleanOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BooleanOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insure Application");
+
+            Console.Write("What is your age?: ");
+            string userAge = Console.ReadLine();
+
+            Console.Write("Have you ever had a DUI? Please answer 'true' or 'false': ");
+            string userDuiStatus = Console.ReadLine();
+
+            Console.Write("How many speeding tickets do you have?: ");
+            string userTicketCount = Console.ReadLine();
+
+            bool qualifier = Convert.ToInt32(userAge) > 15 && Convert.ToBoolean(userDuiStatus) == false && Convert.ToInt32(userTicketCount) <= 3;
+
+            Console.WriteLine("Are you qualified?: "+qualifier);
+
+
+        }
+    }
+}
